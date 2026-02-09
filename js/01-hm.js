8dbeacfed42d9493136d3f95d0785d268cfbb435
Daniel <puricchdaniil@gmail.com>
2026-02-09 23:56:53 +0200

    add all of the staff (change number, myNull, and others

--- js/01-hm.js
+++ js/01-hm.js
@@ -22,3 +22,33 @@ console.log("Is student:", isStudent);
 let myString = "The only way to do great work is to love what you do.";
 console.log("Quote:", myString);
 
+/**
+ * 5. Змінна myNumber (додавання числа)
+ */
+let myNumber = 42;
+myNumber = myNumber + 10;
+console.log("My Number (+10):", myNumber);
+
+/**
+ * 6. Змінна myNull
+ */
+let myNull = null;
+console.log("My Null:", myNull);
+
+/**
+ * 7. Взаємодія з користувачем: prompt
+ */
+let userName = prompt("Введіть своє ім’я");
+console.log("Type of userName:", typeof userName);
+
+/**
+ * 8. Підтвердження: confirm
+ */
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("User answer (confirm):", userAnswer);
+
+/**
+ * 9. Попередження та небезпечна дія
+ */
+alert("Увага! Дія може бути небезпечною.");
+let isConfirmed = confirm("Ви впевнені, що хочете продовжити?");

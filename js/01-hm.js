cbf567b103226e4d7a618430d417100414a1f933
Daniel <puricchdaniil@gmail.com>
2026-02-09 23:55:02 +0200

    Add change age, name, status, myString

---
+++ js/01-hm.js
@@ -0,0 +1,24 @@
+/**
+ * 1. Змінна age (вік)
+ */
+let age = 25; // Приклад значення
+console.log("Age:", age);
+
+/**
+ * 2. Змінна name (ім'я)
+ */
+let name = "Gemini";
+console.log("Name:", name);
+
+/**
+ * 3. Змінна isStudent (статус студента)
+ */
+let isStudent = true;
+console.log("Is student:", isStudent);
+
+/**
+ * 4. Змінна myString (улюблена цитата)
+ */
+let myString = "The only way to do great work is to love what you do.";
+console.log("Quote:", myString);
+
